fe4fbd22e6433b88395cedd32e28b7080bee5ba7
GitHubshimaa <shimaasabry1230@gmail.com>
2025-07-30 11:13:28 +0300

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
 function myfun(){
     alert("Welcome")
-    window.open('selectType.html',target="_blanck")
+    window.open('selectType.html',target="_blank")
 }
 
 
 
 function myfunction(){
     alert("Welcome To BMW cars");
-    window.open('BMW.html',target="_blanck")
+    window.open('BMW.html',target="_blank")
 
 }
 function myfunction2(){
     alert("Welcome To Toyota cars");
-    window.open('Toyota.html',target="_blanck")
+    window.open('Toyota.html',target="_blank")
 }
