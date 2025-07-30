7041d604a320cee2ad3992940707766a4940aaf5
GitHubshimaa <shimaasabry1230@gmail.com>
2025-07-30 10:41:26 +0300

    Add files via upload
    CarStore

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function myfun(){
+    alert("Welcome")
+    window.open('selectType.html',target="_blanck")
+}
+
+
+
+function myfunction(){
+    alert("Welcome To BMW cars");
+    window.open('BMW.html',target="_blanck")
+
+}
+function myfunction2(){
+    alert("Welcome To Toyota cars");
+    window.open('Toyota.html',target="_blanck")
+}
